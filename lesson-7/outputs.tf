8f55470ecd6464f0a2015bae83538c3f764bd4fa
Alex Kydanov <a.kidanov@gmail.com>
2025-07-19 19:19:54 +0300

    Lesson 7

---
+++ lesson-7/outputs.tf
@@ -0,0 +1,44 @@
+output "ecr_repository_arn" {
+  description = "ARN of the created repository."
+  value       = module.ecr.repository_arn
+}
+
+output "ecr_repository_url" {
+  description = "Full URL (hostname/name) for docker push/pull."
+  value       = module.ecr.repository_url
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS API endpoint for connecting to the cluster"
+  value       = module.eks.eks_cluster_endpoint
+}
+
+output "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.eks_cluster_name
+}
+
+output "eks_node_role_arn" {
+  description = "IAM role ARN for EKS Worker Nodes"
+  value       = module.eks.eks_node_role_arn
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = module.vpc.internet_gateway_id
+}
+
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
